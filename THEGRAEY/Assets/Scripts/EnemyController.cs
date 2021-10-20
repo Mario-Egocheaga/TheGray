41548109b74e937adb71f664aff5c15ad14b9327
Danny <danchristie991@gmail.com>
2021-10-19 23:30:56 -0400

    reee

--- THEGRAEY/Assets/Scripts/EnemyController.cs
+++ THEGRAEY/Assets/Scripts/EnemyController.cs
@@ -8,28 +8,41 @@ public class EnemyController : MonoBehaviour
     private GameObject player;
     private bool playerSpotted;
     private float moveSpeed;
+    private int patrolTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 5f;
+        moveSpeed = 2f;
+        patrolTimer = 500;
         player = GameObject.FindGameObjectWithTag("Player");
         playerSpotted = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(playerSpotted)
-        {   
-            //rotate to look at player
+        {
+            moveSpeed = 7.5f;
+            //Rotate to look at player
             transform.LookAt(player.transform.position);
-            //move towards player
+            //Move towards player
             transform.position += transform.forward * moveSpeed * Time.deltaTime;
         }
         else
         {
-            
+            if(patrolTimer < 0)
+            { 
+                transform.LookAt(new Vector3(Random.Range(-40, 41), 10, Random.Range(-40, 41))); //Patrol a 40x40 area
+                patrolTimer = 500;
+            }
+            else
+            {
+                moveSpeed = 2f;
+                transform.position += transform.forward * moveSpeed * Time.deltaTime; //Move to position
+                patrolTimer--;
+            }
         }
     }
 
