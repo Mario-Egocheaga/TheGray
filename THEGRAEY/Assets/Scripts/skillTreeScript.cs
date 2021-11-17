1b0bc89d95c6d2c91b6b0cc93218da1805266d07
Callum1scott <61849378+Callum1scott@users.noreply.github.com>
2021-11-17 13:37:33 -0500

    Working prototype skill tree
    Working radial menu

--- THEGRAEY/Assets/Scripts/skillTreeScript.cs
+++ THEGRAEY/Assets/Scripts/skillTreeScript.cs
@@ -5,25 +5,109 @@ using UnityEngine.UI;
 
 public class skillTreeScript : MonoBehaviour
 {
-    public Text Attribute;
-    public Text Name;
-    public Text Desc;
-    public GameObject Selector;
+    //UI Mouse position VAriable
+    public Vector2 moveInput;
 
-    public Vector3 MosPos;
+    public Image CanvasImageUpLeft;
+    public Image CanvasImageLeft;
+    public Image CanvasImageUpRight;
+    public Image CanvasImageRight;
+    public Image CanvasImagebottom;
+    public Sprite Empty;
+    public Sprite Red;
+    public Sprite Yellow;
+    public Sprite Purple;
+    public Sprite Green;
+    public Sprite Blue;
+
+    public Text skillType;
+    public Text skillName;
+    public Text skillDescript;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        MosPos = Input.mousePosition;
+        moveInput.x = Input.mousePosition.x - (Screen.width / 2f);
+        moveInput.y = Input.mousePosition.y - (Screen.height / 2f);
+        moveInput.Normalize();
 
-        Selector.transform.rotation = Quaternion.Euler(MosPos);
-        Debug.Log(Input.mousePosition);
+        
+
+        //Radial MenuSelection
+        if (moveInput != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveInput.y, -moveInput.x);
+           
+
+            //Location If's tracking mouse position
+            if (angle > .63 && angle < 1.56)
+            {
+
+                Debug.Log("Top Left");
+                CanvasImageUpLeft.sprite = Red;
+                CanvasImagebottom.sprite = Empty;
+                CanvasImageUpRight.sprite = Empty;
+                CanvasImageRight.sprite = Empty;
+                CanvasImageLeft.sprite = Empty;
+
+                skillType.text = "Dashing";
+
+            }
+            else if (angle > 1.56 && angle < 2.5)
+            {
+                CanvasImageUpLeft.sprite = Empty;
+                CanvasImagebottom.sprite = Empty;
+                CanvasImageUpRight.sprite = Yellow;
+                CanvasImageRight.sprite = Empty;
+                CanvasImageLeft.sprite = Empty;
+                Debug.Log("Top Right");
+
+                skillType.text = "Jumping";
+            }
+            else if (angle > 2.5 || angle < -2.47)
+            {
+                CanvasImageUpLeft.sprite = Empty;
+                CanvasImagebottom.sprite = Empty;
+                CanvasImageUpRight.sprite = Empty;
+                CanvasImageRight.sprite = Purple;
+                CanvasImageLeft.sprite = Empty;
+                Debug.Log("right");
+
+                skillType.text = "Sliding";
+            }
+            else if (angle > -2.5 && angle < -.64)
+            {
+                CanvasImageUpLeft.sprite = Empty;
+                CanvasImagebottom.sprite = Green;
+                CanvasImageUpRight.sprite = Empty;
+                CanvasImageRight.sprite = Empty;
+                CanvasImageLeft.sprite = Empty;
+                Debug.Log("bottom");
+
+
+                skillType.text = "Crouching";
+            }
+            else if (angle > -.64 && angle < .63)
+            {
+                CanvasImageUpLeft.sprite = Empty;
+                CanvasImagebottom.sprite = Empty;
+                CanvasImageUpRight.sprite = Empty;
+                CanvasImageRight.sprite = Empty;
+                CanvasImageLeft.sprite = Blue;
+                Debug.Log("Left");
+
+                skillType.text = "Wall-Running";
+            }
+            
+        }
+        
+        
     }
 }
