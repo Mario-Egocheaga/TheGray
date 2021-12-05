f781ade57772c3933699fc5c3fcdc5b298ec55b9
Callum1scott <61849378+Callum1scott@users.noreply.github.com>
2021-12-04 23:36:46 -0500

    UI no bad noi more

--- THEGRAEY/Assets/Scripts/ToggleScript.cs
+++ THEGRAEY/Assets/Scripts/ToggleScript.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class ToggleScript : MonoBehaviour
 {
     public GameObject skillTree;
-
+    public GameObject HUD;
+    public GameObject MiniMap;
+    public GameObject Camera;
+    
     // Start is called before the first frame update
     void Start()
     {
         skillTree.SetActive(false);
+        
     }
 
     // Update is called once per frame
@@ -19,11 +23,18 @@ public class ToggleScript : MonoBehaviour
         {
             skillTree.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
+
+            HUD.SetActive(false);
+            MiniMap.SetActive(false);
+           Camera.GetComponent<CameraController>().enabled = false;
         }
         else
         {
             skillTree.SetActive(false);
             Cursor.lockState = CursorLockMode.Locked;
+            HUD.SetActive(true);
+            MiniMap.SetActive(true);
+            Camera.GetComponent<CameraController>().enabled = true;
         }
     }
 }
